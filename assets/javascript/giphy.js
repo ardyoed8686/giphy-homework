717f6afab1cc879ac3e7d7d2bc4ff3b5d10fa152
Romy Owens <rdoed160@gmail.com>
2019-10-07 20:42:34 -0400

    added ajax get call

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
     // array of movies
 
@@ -13,31 +13,83 @@ $(document).ready(function(){
     "The Avengers",
     "Thor"];
 
-    //  generate buttons for items in movies array
-    function renderButtons() {
+    function displayMovieGif() {
 
-//     Your app should take the topics in this array and create buttons in your HTML.
-//    * Try using a loop that appends a button for each string in the array.
+        var inputText = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + "The+Dark+Knight" + "&apikey=UIrioujrYNuoTPwZbtRtWOGp4prBx4Rs";
 
-    $("#movie-buttons-view").text("");
+        // Creates AJAX call for the specific movie button being clicked
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
 
-    for (let i = 0; i < topics.length; i++) {
-        // create a button
-        var movieButton = $("<button>");
+            // YOUR CODE GOES HERE!!!
+            console.log(response);
+            // Add a movie div to the #movie-view div
+            // var inputTextDiv = $('<div class="movie-display">');
 
-        movieButton.addClass("topics");
+            // Add a poster to our movie div
+            // var image = $("<img>").attr("src", response.Poster);
+            // inputTextDiv.append(image);
 
-        movieButton.attr("data-search", topics[i]);
+            // Add release date
+            // var releaseDate = $("<p>").text(response.Released);
+            // inputTextDiv.append(releaseDate);
 
-        movieButton.text(topics[i]);
+            // Add rating
+            // var rating = $("<p>").text(JSON.stringify(response.Ratings));
+            // inputTextDiv.append(rating);
 
-        $("#movie-buttons-view").append(movieButton);
-        console.log(topics[i]);
-        }
+            // Add plot
+            // var plot = $("<p>").text(response.Plot);
+            // inputTextDiv.append(plot);
 
+            // Add the movie to the top of the movies view
+            // $("#movies-view").prepend(inputTextDiv);
+
+
+
+        });
 
     }
 
+
+
+      // Function for displaying movie data
+      function renderButtons() {
+
+        // Deleting the movie buttons prior to adding new movie buttons
+        // (this is necessary otherwise we will have repeat buttons)
+        $("#movie-buttons-view").empty();
+
+        // Looping through the array of movies
+        for (var i = 0; i < topics.length; i++) {
+
+          // Dynamicaly generate buttons for each movie in the array.
+          
+          var a = $("<button>");
+          // Adding a class
+          a.addClass("movie");
+          // Adding a data-attribute with a value of the movie at index i
+          a.attr("data-name", topics[i]);
+          // Providing the button's text with a value of the movie at index i
+          a.text(topics[i]);
+          // Adding the button to the HTML
+          $("#movie-buttons-view").append(a);
+        }
+      }
+
+      //adding new buttons
+    $("#add-movie-btn").on("click", function(event) { 
+    event.preventDefault();
+
+        //grab html from what is typed into box
+        var inputText = $("#movie-input").val().trim();
+        topics.push(inputText);
+        renderButtons();
+        console.log(topics);
+    });
 
 
 });
